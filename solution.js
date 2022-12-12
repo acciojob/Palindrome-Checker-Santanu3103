86ae342e296a0446b264d778874f7c9eaca6b060
santunu <santanuroy253396@gmail.com>
2022-12-12 13:43:21 +0530

    first commit

--- solution.js
+++ solution.js
@@ -1,6 +1,10 @@
 // complete the given function
 
 function palindrome(str){
+ let regularexpressions = /[\W_]/g;
+ let transformInLowerCase = str.toLowerCase().replace(regularexpressions,'');
+ let reverseString = transformInLowerCase.split('').reverse().join('');
 
+   return (reverseString === transformInLowerCase);
 }
 module.exports = palindrome
